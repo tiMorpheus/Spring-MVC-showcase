c730c414757cb5501fbaa5042ee08df0f61860f6
Tima Tolochko <timur10645@icloud.com>
2016-10-03 18:13:25 +0300

    spring boot "Hello world example"

---
+++ SpringBoot/src/main/java/Example.java
@@ -0,0 +1,23 @@
+
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home(){
+        return "Hello World!!!";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+}
