97a01d134db2811c78896d0d72fd32031b9fffb7
Tima Tolochko <timur10645@icloud.com>
2016-10-03 21:05:32 +0300

    init , set model

---
+++ spring-boot-sample-data-rest/src/main/java/sample/data/rest/SampleDataRestApplication.java
@@ -0,0 +1,4 @@
+package sample.data.rest;
+
+public class SampleDataRestApplication {
+}
---
+++ spring-boot-sample-data-rest/src/main/java/sample/data/rest/domain/City.java
@@ -0,0 +1,55 @@
+package sample.data.rest.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class City implements Serializable{
+
+    private static final long serialVersionUID = 4475421814613801058L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String map;
+
+    public City(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getMap() {
+        return map;
+    }
+
+    @Override
+    public String toString() {
+        return getName()+", " + getState() + "," + getCountry();
+    }
+}
